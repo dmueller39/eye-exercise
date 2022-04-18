9d6bf9daacbd75ef2d776d5d07c93c9880ff7d81
Daniel Mueller <danielmueller@uwalumni.com>
2022-04-18 17:24:24 -0400

    minor fixes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Eye Exercise
 
+https://dmueller39.github.io/eye-exercise/
+
 For use with a phone. Load the eye exercise on your browser and hold the base against the bridge of your nose. Point the top of the phone away from you. Align the top circles together, so that the lines are diverging from the top. Hold the circles together in your eyesight for 30 secs.
--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,21 @@ import logo from "./exercise_graphic.svg";
 import "./App.css";
 
 function App() {
+  const onDone = () => {
+    if (window != null && window.top != null) {
+      const message = window.location.href + ";complete;-";
+      console.log(message);
+      window.top.postMessage(message, "*");
+    }
+  };
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
+      <div className="App-button-wrapper">
+        <button className="App-button" type="button" onClick={onDone}>
+          Done
+        </button>
+      </div>
     </div>
   );
 }
